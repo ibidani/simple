2ac2dff06143b8f9edaeca96d50f990d48f1e378
John Krupka <john.krupka@united.com>
2013-12-06 12:15:27 -0600

    complete sample of EAR packaging Groovy MDB, using POM inheritance

---
+++ simpleapp.mdbear/src/main/java/simpleapp/mdbear/App.java
@@ -0,0 +1,9 @@
+package simpleapp.mdbear;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- simpleapp.mdbs/src/main/groovy/simpleapp/mdbs/GroovyMdb.groovy
+++ simpleapp.mdbs/src/main/groovy/simpleapp/mdbs/GroovyMdb.groovy
@@ -1,17 +1,22 @@
 package simpleapp.mdbs
 
+import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
-
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
-@MessageDriven (mappedName="jms/q0")
-class GroovyMdb implements MessageListener {
+@MessageDriven(activationConfig = [
+	@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+	@ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/q0")],
+	mappedName = "jms/q0")
+public class GroovyMdb implements MessageListener {
+	public GroovyMdb() {}
+	
 	public void onMessage(Message message) {
 		if (message instanceof TextMessage) {
-			println "$this onMessage: ${((TextMessage)message).text}"
+			println "$this received TextMessage: ${((TextMessage)message).text}"
 		}
 	}
 }
